a09ec8f1275bcb1d369607e632ae0c2692161ab2
alexfont <alexfont321@gmail.com>
2018-07-12 12:02:40 -0500

    some more stuff with OE

--- overlyExcited/main.js
+++ overlyExcited/main.js
@@ -6,46 +6,85 @@ let sentence = ["The", "walrus", "danced", "through", "the", "trees", "in", "the
     the array as the sole argument. It should iterate over the array
     and output the words to the browser console.
 */
-function addExcitement(theWordArray) {
 
-    // Each time the for loop executes, you're going to add one more word to this string
+//ADDING CONDITIONS TO THE LOOP
+
+// function addExcitement(theWordArray) {
+
+//     // Each time the for loop executes, you're going to add one more word to this string
+//     let buildMeUp = "";
+//     let emptyArray = [];
+//     for (let i = 0; i < theWordArray.length; i++) {
+//         // Concatenate the new word onto buildMeUp
+//         buildMeUp += ` ${theWordArray[i]}`;
+//         emptyArray.push(buildMeUp);
+//         if (emptyArray.length % 3 === 0) {
+//             buildMeUp += "!";
+//             console.log(buildMeUp)
+//         // } else if (emptyArray.length === 6) {
+//         //     console.log(buildMeUp + "!");
+//         } else {
+//             console.log(buildMeUp)
+//         }
+//     }
+// }
+
+//ADDING THE MULITPLE EXCLAMATION POINTS
+
+// function addExcitement(theWordArray) {
+//     let buildMeUp = "";
+//     let emptyArray = [];
+
+//     for (let i = 0; i < theWordArray.length; i++) {
+//         buildMeUp += ` ${theWordArray[i]}`;
+//         emptyArray.push(buildMeUp);
+//         if (emptyArray.length % 3 === 0 && emptyArray.length / 3 === 1) {
+//             buildMeUp += "!";
+//             console.log(buildMeUp)
+//         } else if (emptyArray.length % 3 === 0 && emptyArray.length / 3 === 2) {
+//             buildMeUp += "!!";
+//             console.log(buildMeUp)
+//         } else if (emptyArray.length % 3 === 0 && emptyArray.length / 3 === 3) {
+//             buildMeUp += "!!!";
+//             console.log(buildMeUp)
+//         } else if (emptyArray.length % 3 === 0 && emptyArray.length / 3 === 4) {
+//             buildMeUp += "!!!!";
+//             console.log(buildMeUp)
+//         }
+//         else {
+//             console.log(buildMeUp)
+//         }
+//     }
+// }
+
+function addExcitement(theWordArray, mark) {
     let buildMeUp = "";
     let emptyArray = [];
+
     for (let i = 0; i < theWordArray.length; i++) {
-        // Concatenate the new word onto buildMeUp
         buildMeUp += ` ${theWordArray[i]}`;
         emptyArray.push(buildMeUp);
-        if (emptyArray.length % 3 === 0) {
-            buildMeUp += "!";
+        if (emptyArray.length % 3 === 0 && emptyArray.length / 3 === 1) {
+            buildMeUp += mark;
             console.log(buildMeUp)
-        // } else if (emptyArray.length === 6) {
-        //     console.log(buildMeUp + "!");
-        } else {
+        } else if (emptyArray.length % 3 === 0 && emptyArray.length / 3 === 2) {
+            buildMeUp += mark + mark;
+            console.log(buildMeUp)
+        } else if (emptyArray.length % 3 === 0 && emptyArray.length / 3 === 3) {
+            buildMeUp += mark;
+            console.log(buildMeUp)
+        } else if (emptyArray.length % 3 === 0 && emptyArray.length / 3 === 4) {
+            buildMeUp += mark;
+            console.log(buildMeUp)
+        }
+        else {
             console.log(buildMeUp)
         }
     }
 }
 
 
-    //     // Print buildMeUp to the console
-    //     if (buildMeUp.length % 3 === 0) {
-    //         console.log(buildMeUp + '!');
-    //     } else {
-    //     }
-    // }
-
-    // for (let i = 0; i < theWordArray.length; i++) {
-    //     // Concatenate the new word onto buildMeUp
-    //     buildMeUp += `${sentence[i]}`;
-    //     // Print buildMeUp to the console
-    // }
-    // if (buildMeUp.length % 3 === 0) {
-    //     console.log(`${buildMeUp}! `);
-    // } else {
-    //     console.log(`${buildMeUp} `);
-    // }
-
 
 
 // Invoke the function and pass in the array
-addExcitement(sentence);
+addExcitement(sentence, "?");
